068a384ced7f2b0a22e148c74b6c7ae4de1cfa5b
Iván Ovejero <ivov.src@gmail.com>
2025-01-28 12:13:19 +0100

    chore: Format queries consistently

--- queries/repopulate-analytics-by-compaction.sql
+++ queries/repopulate-analytics-by-compaction.sql
@@ -12,30 +12,30 @@ BEGIN TRANSACTION;
 
 INSERT INTO analytics_by_period (workflowId, type, count, periodUnit, periodStart)
 SELECT 
-    workflowId,
-    type,
-    CASE
-        WHEN type IN ('success', 'failure') THEN COUNT(*)
-        WHEN type = 'time_saved_min' THEN SUM(value)
-        WHEN type = 'runtime_ms' THEN SUM(value)
-    END as count,
-    'hour' as periodUnit,
-    strftime('%Y-%m-%d %H:00:00', timestamp) as periodStart
+  workflowId,
+  type,
+  CASE
+    WHEN type IN ('success', 'failure') THEN COUNT(*)
+    WHEN type = 'time_saved_min' THEN SUM(value)
+    WHEN type = 'runtime_ms' THEN SUM(value)
+  END as count,
+  'hour' as periodUnit,
+  strftime('%Y-%m-%d %H:00:00', timestamp) as periodStart
 FROM analytics
 WHERE timestamp >= datetime('now', '-30 days')
 GROUP BY workflowId, type, strftime('%Y-%m-%d %H:00:00', timestamp);
 
 INSERT INTO analytics_by_period (workflowId, type, count, periodUnit, periodStart)
 SELECT 
-    workflowId,
-    type,
-    CASE
-        WHEN type IN ('success', 'failure') THEN COUNT(*)
-        WHEN type = 'time_saved_min' THEN SUM(value)
-        WHEN type = 'runtime_ms' THEN SUM(value)
-    END as count,
-    'day' as periodUnit,
-    date(timestamp) as periodStart
+  workflowId,
+  type,
+  CASE
+    WHEN type IN ('success', 'failure') THEN COUNT(*)
+    WHEN type = 'time_saved_min' THEN SUM(value)
+    WHEN type = 'runtime_ms' THEN SUM(value)
+  END as count,
+  'day' as periodUnit,
+  date(timestamp) as periodStart
 FROM analytics
 WHERE timestamp < datetime('now', '-30 days') 
 AND timestamp >= datetime('now', '-90 days')
@@ -43,15 +43,15 @@ GROUP BY workflowId, type, date(timestamp);
 
 INSERT INTO analytics_by_period (workflowId, type, count, periodUnit, periodStart)
 SELECT 
-    workflowId,
-    type,
-    CASE
-        WHEN type IN ('success', 'failure') THEN COUNT(*)
-        WHEN type = 'time_saved_min' THEN SUM(value)
-        WHEN type = 'runtime_ms' THEN SUM(value)
-    END as count,
-    'week' as periodUnit,
-    date(timestamp, 'weekday 0', '-7 days') as periodStart
+  workflowId,
+  type,
+  CASE
+    WHEN type IN ('success', 'failure') THEN COUNT(*)
+    WHEN type = 'time_saved_min' THEN SUM(value)
+    WHEN type = 'runtime_ms' THEN SUM(value)
+  END as count,
+  'week' as periodUnit,
+  date(timestamp, 'weekday 0', '-7 days') as periodStart
 FROM analytics
 WHERE timestamp < datetime('now', '-90 days')
 GROUP BY workflowId, type, date(timestamp, 'weekday 0', '-7 days');
